525de6641b67462e74f498f15f1dce522c4d790e
cryptofixyup🔥 <Cryptofixyup@yahoo.com>
2026-08-14 09:21:13 +0200

    fix(core): make durable state transitions authorization-bound

--- sentinel-core/state/store.js
+++ sentinel-core/state/store.js
@@ -1,17 +1,24 @@
-export class InMemoryDurableState {
-  #state;
-  #version;
+import { applyTransition } from '../lifecycle/machine.js';
 
-  constructor(initialState) {
+export class DurableState {
+  #state;
+  #version = 0;
+  #authorizedActors;
+
+  constructor(initialState, authorizedActors) {
     this.#state = structuredClone(initialState);
-    this.#version = 0;
+    this.#authorizedActors = new Set(authorizedActors);
   }
 
-  read() { return Object.freeze({ state: structuredClone(this.#state), version: this.#version }); }
+  read() {
+    return Object.freeze({ state: structuredClone(this.#state), version: this.#version });
+  }
 
-  commit(expectedVersion, nextState) {
-    if (expectedVersion !== this.#version) throw new Error('STALE_STATE');
-    this.#state = structuredClone(nextState);
+  transition(proposal, to) {
+    if (!this.#authorizedActors.has(proposal.actor)) throw new Error('UNAUTHORIZED_ACTOR');
+    const current = { ...this.#state, version: this.#version };
+    const next = applyTransition({ current, proposal, to });
+    this.#state = structuredClone(next);
     this.#version += 1;
     return this.read();
   }
